b9f3961a66e0d592e3c468b63df1e69e01905f7b
Kevin <quevon24@gmail.com>
2016-10-05 22:32:08 -0500

    agregar carta, audio, video

--- mapp/settings.py
+++ mapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '93*9#)fbp(8q%)(8&#tr%xs#r%0@zrz*ip%m4=5%)jkpp#*4(v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
